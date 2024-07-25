5fe335ef326113e57baa90d20cdd9bb18a9795fc
Pumita <hi+git@hackr.es>
2024-07-25 14:31:20 +0200

    aaaa

--- os/flake.nix
+++ os/flake.nix
@@ -11,7 +11,7 @@
   outputs = { nixpkgs, minegrub, ... }:
     let
       lib' = ((import ../lib/flake.nix).outputs { inherit nixpkgs; }).lib;
-      third-party' = { inherit minegrub };
+      third-party' = { inherit minegrub; };
     in {
       nixosConfigurations = lib'.mkHost {
         hostname = "metaverse"; username = "architech";
