e5282c02fa7030301cd5d62b979816b483d8f36f
Pumita <hi+git@hackr.es>
2024-07-25 11:49:10 +0200

    not absolute

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    os' = { url = "./os"; inputs.nixpkgs.follows = "nixpkgs"; };
+    os' = { url = "path:./os"; inputs.nixpkgs.follows = "nixpkgs"; };
   };
 
   outputs = { self, os', ... }: {
