5574c5c81a33dfce45c7609cdd20e1ceb8c96305
Pumita <hi+git@hackr.es>
2024-07-25 23:06:46 +0200

    oopsy...

--- scripts/rebuild.nix
+++ scripts/rebuild.nix
@@ -7,7 +7,7 @@
     }
     # Check if flake is a git repository
     ${pkgs.git}/bin/git -C "$FLAKE" rev-parse --is-inside-work-tree >/dev/null 2>&1 && {
-      ${pkgs.git}/bin/git -C "\${FLAKE}" add -A
+      ${pkgs.git}/bin/git -C "$FLAKE" add -A
     }
-    ${pkgs.nh}/bin/nh os switch -H "$(hostname)-$(uname -m)"
+    ${pkgs.nh}/bin/nh os switch -H "$(hostname)-$(uname -m)" -- $@
   ''
