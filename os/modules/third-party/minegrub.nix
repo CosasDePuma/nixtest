452fcabf5945f2a7e5e839fc5d36ce7d670f8803
Pumita <hi+git@hackr.es>
2024-07-25 14:51:12 +0200

    aaaaaa

--- os/modules/third-party/minegrub.nix
+++
@@ -1,6 +0,0 @@
-{ config, lib, ... }: {
-  # MineGRUB
-  boot.loader.grub.minegrub-theme.enable = lib.mkDefault true;
-  boot.loader.grub.minegrub-theme.splash = "Flakes!";
-  boot.loader.grub.minegrub-theme.boot-options-count = 4;
-}
