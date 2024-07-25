7a15255395ad5a6399958da13389d09380c4c90f
Pumita <hi+git@hackr.es>
2024-07-26 01:58:18 +0200

    oopsy...

--- os/modules/system/nh.nix
+++ os/modules/system/nh.nix
@@ -18,11 +18,11 @@
       in {
         programs.nh.enable = lib.mkDefault true;
         programs.nh.flake = lib.mkDefault cfg.nh;
-      } // lib.mkIf gcEnabled {
+      } /*// lib.mkIf gcEnabled {
         # Garbage collector
         nix.gc.automatic = lib.mkForce false;
         programs.nh.clean.enable = lib.mkDefault true;
         programs.nh.clean.dates = lib.mkDefault cfg.gc;
         programs.nh.clean.extraArgs = lib.mkDefault "--keep 5 --keep-since 7d";
-      });
+      }*/);
   }
