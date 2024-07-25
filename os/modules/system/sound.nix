ae58ec08fe2f24a827b3f93d6160ec43bcd2f250
Pumita <hi+git@hackr.es>
2024-07-25 23:01:48 +0200

    aa

--- os/modules/system/sound.nix
+++ os/modules/system/sound.nix
@@ -15,13 +15,13 @@
 
     config = {
       hardware.pulseaudio.enable = lib.mkDefault (cfg.sound == "pulseaudio");
-      hardware.pipewire = lib.mkIf (cfg.sound == "pipewire") {
+      services.pipewire = lib.mkIf (cfg.sound == "pipewire") {
         enable = lib.mkDefault true;
         pulseaudio.enable = lib.mkDefault true;
         alsa.enable = lib.mkDefault true;
         alsa.support32Bit = lib.mkDefault true;
         jack.enable = lib.mkDefault true;
-        security.rtkit.enable = lib.mkDefault true;
       };
+      security.rtkit.enable = lib.mkDefault (cfg.sound != "none");
     };
 }
--- scripts/rebuild.nix
+++ scripts/rebuild.nix
@@ -1,12 +1,13 @@
 { pkgs, ... }:
   pkgs.writeShellScriptBin "nh-rebuild" ''
-    #!/usr/bin/env sh
+    # Check if nh is installed
     command -v nh >/dev/null 2>&1 || {
       echo "nh is not installed. Aborting." >&2
       exit 1
     }
-    ${pkgs.git}/bin/git -C "$FLAKE" rev-parse --is-inside-work-tree >/dev/null 2>&1 && {
-      ${pkgs.git}/bin/git -C "$FLAKE" add -A
+    # Check if flake is a git repository
+    ${pkgs.git}/bin/git -C "${FLAKE}" rev-parse --is-inside-work-tree >/dev/null 2>&1 && {
+      ${pkgs.git}/bin/git -C "\${FLAKE}" add -A
     }
     ${pkgs.nh}/bin/nh os switch -H "$(hostname)-$(uname -m)"
   ''
