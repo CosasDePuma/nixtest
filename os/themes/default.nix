77f9c1c688a6dafc84ed668259d5a88fe7ce2da5
Pumita <hi+git@hackr.es>
2024-07-26 10:53:05 +0200

    oopsy...

---
+++ os/themes/default.nix
@@ -0,0 +1,24 @@
+{ config, lib, ... }:
+  let
+    cfg = config.within.theme;
+  in {
+    imports = [
+      ../../modules/desktop
+    ];
+    
+    options.within = {
+      theme = lib.mkOption {
+        default = null;
+        example = "cinnamon";
+        type = with lib.types; nullOr (enum ["none" "cinnamon"]);
+        description = ''
+          Desktop environment to use. Theme and dependencies will be installed.
+          Options are: `none`, `cinnamon`.
+        '';
+      };
+    };
+
+    config =
+      if cfg.theme == "cinnamon" then import ./cinnamon { inherit lib; }
+      else {};
+  }
