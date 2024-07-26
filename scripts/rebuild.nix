58d33ec2defce8785873eecf7efc922914d8592b
Pumita <hi+git@hackr.es>
2024-07-26 03:25:58 +0200

    oopsy...

--- scripts/rebuild.nix
+++ scripts/rebuild.nix
@@ -1,13 +1,16 @@
 { pkgs, ... }:
-  pkgs.writeShellScriptBin "nh-rebuild" ''
-    # Check if nh is installed
-    command -v nh >/dev/null 2>&1 || {
-      echo "nh is not installed. Aborting." >&2
-      exit 1
-    }
-    # Check if flake is a git repository
-    ${pkgs.git}/bin/git -C "$FLAKE" rev-parse --is-inside-work-tree >/dev/null 2>&1 && {
-      ${pkgs.git}/bin/git -C "$FLAKE" add -A
-    }
-    ${pkgs.nh}/bin/nh os switch -H "$(hostname)-$(uname -m)" -- $@
+  pkgs.writeShellScriptBin "rebuild" ''
+    # Set the flake path
+    test -n "$FLAKE" || FLAKE="/etc/nixos"
+    # Check the system configuration
+    CONFIG="$(${pkgs.net-tools}/bin/hostname)-$(${pkgs.coreutils-full}/bin/uname -m)"
+    # Check which update method is available
+    REBUILD="${pkgs.sudo}/bin/sudo ${pkgs.nixos-rebuild}/bin/nixos-rebuild switch --flake $FLAKE#$CONFIG $@"
+    command -v nh >/dev/null 2>&1 && REBUILD="${pkgs.nh}/bin/nh os switch -H $CONFIG -- $@"
+    # Check if flake is a git repository, add changes, switch to the new system, and restore changes
+    ISGIT="$(${pkgs.git}/bin/git -C "$FLAKE" rev-parse --is-inside-work-tree >/dev/null 2>&1 ; echo "$?")"
+    ${pkgs.coreutils-full}/bin/test "$ISGIT" -eq 0 && ${pkgs.git}/bin/git -C "$FLAKE" add -A
+     os switch -H "$(hostname)-$(uname -m)" -- "$@"
+    ${pkgs.coreutils-fullc}/bin/test "$ISGIT" -eq 0 && ${pkgs.git}/bin/git -C "$FLAKE" restore
   ''
+  aa
