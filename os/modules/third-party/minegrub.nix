48ab17bb88aee1bf93bc46941226f983105a713c
Pumita <hi+git@hackr.es>
2024-07-26 02:36:12 +0200

    oopsy...

--- os/modules/third-party/minegrub.nix
+++
@@ -1,6 +0,0 @@
-{ config, lib, ... }: { # TODO: Move to GRUB module as an extra (?)
-  # MineGRUB: Minecraft GRUB theme
-  boot.loader.grub.minegrub-theme.enable = lib.mkDefault true;
-  boot.loader.grub.minegrub-theme.splash = lib.mkDefault "";
-  boot.loader.grub.minegrub-theme.boot-options-count = lib.mkDefault 4;
-}
