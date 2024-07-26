63189a80873ba0afa91b2853ed2f52d28d59e607
Pumita <hi+git@hackr.es>
2024-07-26 03:43:53 +0200

    aaa

---
+++ os/modules/system/user.nix
@@ -0,0 +1,33 @@
+{ config, lib, ... }:
+  let
+    cfg = config.within;
+  in {
+    options.within = {
+      user = {
+        name = lib.mkOption {
+          default = "user";
+          example = "god";
+          type = lib.types.passwdEntry;
+          description = ''
+            The name of the user.
+          '';
+        };
+      };
+    };
+
+    config = {
+      # User
+      users.mutableUsers = lib.mkDefault true;
+      users.users."${cfg.user.name}" = {
+        isNormalUser = lib.mkDefault true;
+        description = lib.mkDefault "Main user";
+        useDefaultShell = lib.mkDefault true;
+        createHome = lib.mkDefault true;
+        home = lib.mkDefault "/home/${cfg.user.name}";
+        group = lib.mkDefault "users";
+        extraGroups = lib.mkDefault [ "docker" "networkmanager" "wheel" ];
+        isSystemUser = lib.mkDefault false;
+        initialPassword = lib.mkDefault "Changeme123!";
+      };
+    };
+}
