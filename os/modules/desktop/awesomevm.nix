223a494a73d75f461d63b5ac39be98ece6d47c2e
Pumita <hi+git@hackr.es>
2024-07-26 04:15:51 +0200

    oopsy...

--- os/modules/desktop/awesomevm.nix
+++ os/modules/desktop/awesomevm.nix
@@ -30,31 +30,3 @@
       ]);
     };
   }
-  options.within.desktop = {
-    awesomevm = {
-      enable = lib.mkOption {
-        default = false;
-        example = true;
-        type = lib.types.bool;
-        description = ''
-          Enable the AwesomeVM.
-        '';
-      };
-    };
-  };
-
-  config = lib.mkIf (cfg.awesomevm.enable) {
-    # Xorg
-    services.xserver.enable = lib.mkDefault true;
-    services.libinput.enable = lib.mkDefault true;
-
-    # Display manager
-    services.displayManager.defaultSession = lib.mkDefault "none+awesome";
-
-    # AwesomeVM
-    programs.xserver.windowManager.awesome.enable = lib.mkDefault true;
-    services.xserver.windowManager.awesome.luaModules = lib.mkDefault (with pkgs.luaPackages; [
-      luarocks luadbi-mysql # FIXME: Maybe luadbi-mysql is not necessary
-    ]);
-  };
-}
