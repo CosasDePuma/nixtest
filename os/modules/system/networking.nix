b0256a7fcb197c4675a55e51a681f2c6a03a2e1d
Pumita <hi+git@hackr.es>
2024-07-26 02:02:10 +0200

    oopsy...

--- os/modules/system/networking.nix
+++ os/modules/system/networking.nix
@@ -41,8 +41,8 @@
       networking.useDHCP = lib.mkDefault false;
       networking.networkmanager.enable = lib.mkDefault true;
       programs.nm-applet.enable = lib.mkDefault true;
-    } // lib.mkIf cfg.network.wifi {
+    }/*// lib.mkIf cfg.network.wifi {
       # WPA_Supplicant
       networking.wireless.enable = lib.mkDefault true;
-    };
+    };*/
   }
