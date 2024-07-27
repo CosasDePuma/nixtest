8f97d21095ee505514ddffc9f1c8f79c2b025b3a
Pumita <hi+git@hackr.es>
2024-07-27 08:20:05 +0200

    oopsy...

--- os/themes/default.nix
+++ os/themes/default.nix
@@ -16,7 +16,7 @@
       };
     };
 
-    config.private = {
+    config = {
       networking.hostName = if cfg.theme == "cinnamo" then "cinnamoroll" else config.private.networking.hostName;
       #cinnamo.enable = (cfg.theme == "cinnamo");
     };
