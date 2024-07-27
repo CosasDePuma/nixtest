be53e0176af27b95d786bd4e70db8c3e5941c376
Pumita <hi+git@hackr.es>
2024-07-27 07:50:00 +0200

    oopsy...

--- os/modules/desktop/awesomevm.nix
+++ os/modules/desktop/awesomevm.nix
@@ -31,17 +31,17 @@
     config = lib.mkIf (cfg.awesomevm.enable) (
       lib.mkMerge [{
         # Xorg
-        services.xserver.enable = lib.mkDefault true;
-        services.libinput.enable = lib.mkDefault true;
+        #services.xserver.enable = lib.mkDefault true;
+        #services.libinput.enable = lib.mkDefault true;
 
         # Display manager
-        services.displayManager.defaultSession = lib.mkDefault "none+awesome";
+        #services.displayManager.defaultSession = lib.mkDefault "none+awesome";
 
         # AwesomeVM
-        services.xserver.windowManager.awesome.enable = lib.mkDefault true;
-        services.xserver.windowManager.awesome.luaModules = lib.mkDefault (with pkgs.luaPackages; [
-          luarocks luadbi-mysql # FIXME: Maybe luadbi-mysql is not necessary
-        ]);
+        #services.xserver.windowManager.awesome.enable = lib.mkDefault true;
+        #services.xserver.windowManager.awesome.luaModules = lib.mkDefault (with pkgs.luaPackages; [
+        #  luarocks luadbi-mysql # FIXME: Maybe luadbi-mysql is not necessary
+        #]);
       } (lib.mkIf (cfg.awesomevm.rc != null) {
         #environment.etc."xdg/awesome/rc.lua".text = lib.mkDefault cfg.awesomevm.rc;
       })
