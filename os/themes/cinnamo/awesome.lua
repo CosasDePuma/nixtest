880aaa24939ab70d4c43c1483386544b1978fc15
Pumita <hi+git@hackr.es>
2024-07-27 08:28:49 +0200

    oopsy...

--- os/themes/cinnamo/awesome.lua
+++ os/themes/cinnamo/awesome.lua
@@ -1,73 +1,63 @@
--- -------------------------------
---   Imports
--- -------------------------------
-
--- import `luarocks` if available
+-- If LuaRocks is installed, make sure that packages installed through it are
+-- found (e.g. lgi). If LuaRocks is not installed, do nothing.
 pcall(require, "luarocks.loader")
--- std awesome library
+
+-- Standard awesome library
 local gears = require("gears")
 local awful = require("awful")
 require("awful.autofocus")
--- widget and layout library
+-- Widget and layout library
 local wibox = require("wibox")
--- theme handling library
+-- Theme handling library
 local beautiful = require("beautiful")
--- notification library
+-- Notification library
 local naughty = require("naughty")
 local menubar = require("menubar")
 local hotkeys_popup = require("awful.hotkeys_popup")
--- enable hotkeys help widget for VIM and other apps
+-- Enable hotkeys help widget for VIM and other apps
+-- when client with a matching name is opened:
 require("awful.hotkeys_popup.keys")
 
--- -------------------------------
---   Variables
--- -------------------------------
-
-editor   = os.getenv("EDITOR")   or "${pkgs.neovim}/bin/nvim"
-terminal = os.getenv("TERMINAL") or "${pkgs.kitty}/bin/kitty"
-nixoscfg = os.getenv("FLAKE")    or "/etc/nixos"
-modkey   = "Mod4" -- Super key (Mod1 is Alt key)
-
--- -------------------------------
---   Context Menu
--- -------------------------------
-
-contextmenu = awful.menu({ items = {
-    { "terminal", terminal },
-    { "cheatsheet", function() hotkeys_popup.show_help(nil, awful.screen.focused()) end },
-    { "edit config", terminal .. " -e " .. editor .. " " .. nixoscfg }, -- edit NixOS config. If no-nix, file should be `awesome.conffile`
-    { "refresh", awesome.restart },
-    { "logout", function() awesome.quit() end }
-}})
-
-mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon,
-                                    menu = contextmenu })
-
--- -------------------------------
---   Error Handling
--- -------------------------------
-
--- handle startup errors
+-- {{{ Error handling
+-- Check if awesome encountered an error during startup and fell back to
+-- another config (This code will only ever execute for the fallback config)
 if awesome.startup_errors then
     naughty.notify({ preset = naughty.config.presets.critical,
-                     title = "(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ Woopsy! There were errors during startup!",
+                     title = "Oops, there were errors during startup!",
                      text = awesome.startup_errors })
 end
 
--- handle runtime errors (after startup)
+-- Handle runtime errors after startup
 do
     local in_error = false
     awesome.connect_signal("debug::error", function (err)
+        -- Make sure we don't go into an endless error loop
         if in_error then return end
         in_error = true
+
         naughty.notify({ preset = naughty.config.presets.critical,
-                         title = "(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ Woopsy! There was an error!",
+                         title = "Oops, an error happened!",
                          text = tostring(err) })
         in_error = false
     end)
 end
+-- }}}
 
+-- {{{ Variable definitions
+-- Themes define colours, icons, font and wallpapers.
+beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 
+-- This is used later as the default terminal and editor to run.
+terminal = "xterm"
+editor = os.getenv("EDITOR") or "nano"
+editor_cmd = terminal .. " -e " .. editor
+
+-- Default modkey.
+-- Usually, Mod4 is the key with a logo between Control and Alt.
+-- If you do not like this or do not have such a key,
+-- I suggest you to remap Mod4 to another key using xmodmap or other tools.
+-- However, you can use another modifier like Mod1, but it may interact with others.
+modkey = "Mod4"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
@@ -92,8 +82,21 @@ awful.layout.layouts = {
 
 -- {{{ Menu
 -- Create a launcher widget and a main menu
+myawesomemenu = {
+   { "hotkeys", function() hotkeys_popup.show_help(nil, awful.screen.focused()) end },
+   { "manual", terminal .. " -e man awesome" },
+   { "edit config", editor_cmd .. " " .. awesome.conffile },
+   { "restart", awesome.restart },
+   { "quit", function() awesome.quit() end },
+}
 
+mymainmenu = awful.menu({ items = { { "awesome", myawesomemenu, beautiful.awesome_icon },
+                                    { "open terminal", terminal }
+                                  }
+                        })
 
+mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon,
+                                     menu = mymainmenu })
 
 -- Menubar configuration
 menubar.utils.terminal = terminal -- Set the terminal for applications that require it
