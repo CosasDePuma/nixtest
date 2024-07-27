86afe1ebf2e9e4d7b36ec4387a1cd1e37d3ff457
Pumita <hi+git@hackr.es>
2024-07-27 07:47:14 +0200

    oopsy...

--- os/themes/cinnamo/default.nix
+++ os/themes/cinnamo/default.nix
@@ -1,7 +1,12 @@
 { config, lib, ... }: let cfg = config.private.cinnamo; in {
   imports = [ ../../modules ];
 
-  options.private.cinnamo.enable = lib.mkEnableOption "cinnamoroll desktop environment";
+  options.private.cinnamo.enable = lib.mkOption {
+    default = false;
+    example = true;
+    type = lib.types.bool;
+    description = "Enable the cinnamoroll desktop environment";
+  };
 
   config = lib.mkIf cfg.enable {
     within.desktop.awesomevm.enable = lib.mkDefault true;
