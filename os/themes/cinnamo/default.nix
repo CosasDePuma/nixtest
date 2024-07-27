6b8f95e2af33f783233234fde25322f754b3338f
Pumita <hi+git@hackr.es>
2024-07-27 08:54:43 +0200

    oopsy...

--- os/themes/cinnamo/default.nix
+++ os/themes/cinnamo/default.nix
@@ -5,8 +5,8 @@
     options._private.theme.cinnamo.enable = lib.mkEnableOption "cinnamoroll desktop environment";
 
     config = lib.mkIf cfg.enable {
-      environment.variables."EDITOR" = pkgs.neovim;
-      environment.variables."TERMINAL" = pkgs.kitty;
+      environment.variables."EDITOR" = "${pkgs.neovim}/bin/nvim";
+      environment.variables."TERMINAL" = "${pkgs.kitty}/bin/kitty";
       within.desktop.awesomevm.enable = lib.mkDefault true;
       within.desktop.awesomevm.rc = lib.mkDefault (builtins.readFile ./awesome.rc);
     };
