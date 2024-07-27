7d98259c4a725ffe89e1f17afb4737b154883a07
Pumita <hi+git@hackr.es>
2024-07-27 08:23:55 +0200

    oopsy...

--- os/themes/cinnamo/default.nix
+++ os/themes/cinnamo/default.nix
@@ -1,10 +1,12 @@
-{ config, lib, ... }: let cfg = config._private.theme.cinnamo; in {
+{ config, lib, ... }:
+  let
+    cfg = config._private.theme.cinnamo;
+  in {
+    options._private.theme.cinnamo.enable = lib.mkEnableOption "cinnamoroll desktop environment";
 
-  options._private.theme.cinnamo.enable = lib.mkEnableOption "cinnamoroll desktop environment";
-
-  config = lib.mkIf cfg.enable {
-    #config.within.desktop.awesomevm.enable = lib.mkDefault true;
-    networking.hostName = "cinnamoroll";
-    #within.desktop.awesomevm.rc = lib.mkDefault (builtins.readFile ./awesome.lua);
-  };
-}
+    config = lib.mkIf cfg.enable {
+      #config.within.desktop.awesomevm.enable = lib.mkDefault true;
+      networking.hostName = "cinnamoroll";
+      #within.desktop.awesomevm.rc = lib.mkDefault (builtins.readFile ./awesome.lua);
+    };
+  }
